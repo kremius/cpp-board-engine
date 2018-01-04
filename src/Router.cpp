10789aa6d4bf636241a2ce6e1c5005c4d6d4bb29
Alex <sajget@ya.ru>
2018-01-05 00:25:38 +0300

    Basic routing implementation

--- src/Router.cpp
+++ src/Router.cpp
@@ -1,20 +1,31 @@
 #include "Router.h"
 
+#include <boost/range/algorithm/find_if.hpp>
+#include <boost/algorithm/string.hpp>
+
 void RouterFactory::onServerStart(folly::EventBase* /*evb*/) noexcept {
-    // Do nothing
+    // TODO: start all factories
 }
 
 void RouterFactory::onServerStop() noexcept {
-    // Do nothing
+    // TODO: stop all factories
 }
 
 proxygen::RequestHandler* RouterFactory::onRequest(
-    proxygen::RequestHandler* /*handler*/,
-    proxygen::HTTPMessage *message) noexcept {
-    // TODO:
-    return nullptr;
+    proxygen::RequestHandler* handler,
+    proxygen::HTTPMessage* message) noexcept {
+    auto it = boost::find_if(routes_, [message](const Route& route) {
+        return boost::starts_with(message->getURL(), route.first);
+    });
+
+    if (it == routes_.end()) {
+        // TODO: 404 handler
+        return nullptr;
+    }
+
+    return it->second->onRequest(handler, message);
 }
 
 void RouterFactory::addRoutes(std::vector<RouterFactory::Route> routes) noexcept {
-    // TODO:
+    routes_ = std::move(routes);
 }
