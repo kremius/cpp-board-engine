2391c2b21804fa473a43a478473f3de87b12f0af
Alex <sajget@ya.ru>
2018-02-01 19:28:22 +0300

    Coverage to coveralls in travis

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -12,4 +12,10 @@ cmake --build .
 # Tests
 ./cpp-board-engine-tests
 
-# TODO: coverage
+# Coverage
+lcov -c -d . -o coverage.info
+lcov --remove coverage.info '/usr/*' --output-file coverage.info
+lcov --remove coverage.info 'test/*' --output-file coverage.info
+lcov --remove coverage.info 'build/*' --output-file coverage.info
+lcov --list coverage.info
+coveralls-lcov --repo-token=${COVERALLS_TOKEN} coverage.info;
