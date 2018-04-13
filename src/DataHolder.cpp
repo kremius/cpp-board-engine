0b24c8d70759bd9606fdfcb52a6ce04f3e0173cf
Alex <sajget@ya.ru>
2018-04-13 15:04:22 +0300

    Boost multi index basic usage

--- src/DataHolder.cpp
+++ src/DataHolder.cpp
@@ -1,1 +1,18 @@
 #include "DataHolder.h"
+
+DataHolder::FuturePosts DataHolder::FetchThreadPosts(uint64_t thread_id)
+{
+    auto&& view = container_.get<tags::thread_post_asc>();
+    auto pair = view.range(
+        [thread_id](auto tuple) { return thread_id <= tuple.value.thread_id; },
+        [thread_id](auto tuple) { return tuple.value.thread_id <= thread_id; });
+
+    folly::fbvector<Record> retval;
+    for (auto it = pair.first; it != pair.second; ++it)
+    {
+        retval.push_back(*it);
+    }
+
+    // TODO
+    return folly::makeFuture(std::move(retval));
+}
--- src/DataHolder.h
+++ src/DataHolder.h
@@ -2,13 +2,56 @@
 
 #include <folly/dynamic.h>
 #include <folly/futures/Future.h>
+#include <folly/FBVector.h>
+
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/composite_key.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/identity.hpp>
+#include <boost/multi_index/member.hpp>
+
+namespace tags
+{
+
+struct thread_post_asc {};
+struct post_asc {};
+
+}
+
+using boost::multi_index::indexed_by;
+using boost::multi_index::ordered_unique;
+using boost::multi_index::hashed_unique;
+using boost::multi_index::tag;
+using boost::multi_index::member;
+using boost::multi_index::composite_key;
 
 class DataHolder
 {
 public:
-    // TODO: should dynamic be used or struct should be returned instead?
-    using FutureDynamic = folly::Future<folly::dynamic>;
+    struct Record
+    {
+        uint64_t post_id;
+        uint64_t thread_id;
+        folly::fbstring text;
+        folly::fbstring image;
+    };
+    using FuturePosts = folly::Future<folly::fbvector<Record>>;
 
-    FutureDynamic FetchThreadPosts(uint64_t thread_id);
+    FuturePosts FetchThreadPosts(uint64_t thread_id);
 private:
+    using DataContainer
+        = boost::multi_index_container<Record,
+            indexed_by<
+                ordered_unique<
+                    tag<tags::thread_post_asc>,
+                    composite_key<
+                        Record,
+                        member<Record, uint64_t, &Record::thread_id>,
+                        member<Record, uint64_t, &Record::post_id>>>,
+                hashed_unique<
+                    tag<tags::post_asc>,
+                    member<Record, uint64_t, &Record::post_id>>>>;
+
+    DataContainer container_;
 };
