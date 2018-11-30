95a64dd2388ec210c0ae1986bdf0abd3801f6f4c
Alex <sajget@ya.ru>
2018-11-30 16:49:12 +0300

    PostHandler now is dereived from BaseHandler

--- src/BoardPostHandler.cpp
+++ src/BoardPostHandler.cpp
@@ -2,6 +2,7 @@
 
 #include <boost/algorithm/string.hpp>
 
+#include <folly/dynamic.h>
 #include <folly/fibers/FiberManagerMap.h>
 #include <folly/json.h>
 
@@ -12,40 +13,12 @@
 using proxygen::ResponseBuilder;
 using proxygen::HTTPHeaderCode;
 
-void BoardPostHandler::onRequest(
-    std::unique_ptr<proxygen::HTTPMessage> headers) noexcept {
-    folly::EventBase* base = folly::EventBaseManager::get()->getEventBase();
+namespace board {
 
-    folly::fibers::getFiberManager(*base).add(
-    [this, headers = std::move(headers)]() mutable {
-        handleRequest(std::move(headers));
-    });
-}
-
-utils::optional<uint64_t> BoardPostHandler::extractThreadNumber(const std::string& url) {
-    if (!boost::starts_with(url, prefix_)) {
-        return {};
-    }
-    const size_t start = prefix_.size();
-    const size_t end = url.size();
-
-    if (end <= start) {
-        return {};
-    }
-
-    folly::fbstring after_perfix(&url[start], end - start);
-    auto maybe_number = folly::tryTo<uint64_t>(after_perfix);
-    if (!maybe_number) {
-        return {};
-    }
-    const uint64_t number = maybe_number.value();
-    return number;
-}
-
-void BoardPostHandler::handleRequest(
+void PostHandler::handleRequest(
     std::unique_ptr<proxygen::HTTPMessage> headers) noexcept {
     const auto& url = headers->getURL();
-    const auto maybe_thread_number = extractThreadNumber(url);
+    const auto maybe_thread_number = utils::extractThreadNumber(url, getPrefix());
     if (!maybe_thread_number) {
         const folly::dynamic value = folly::dynamic::object("thread", "Not Found");
         ResponseBuilder(downstream_)
@@ -65,8 +38,5 @@ void BoardPostHandler::handleRequest(
         .sendWithEOM();
 }
 
-proxygen::RequestHandler* BoardPostHandlerFactory::onRequest(
-    proxygen::RequestHandler*,
-    proxygen::HTTPMessage*) noexcept {
-    return new BoardPostHandler(data_holder_, prefix_);
-}
+} // namespace board
+
--- src/BoardPostHandler.h
+++ src/BoardPostHandler.h
@@ -1,72 +1,22 @@
 #pragma once
 
-#include <proxygen/httpserver/HTTPServer.h>
-#include <proxygen/httpserver/RequestHandlerFactory.h>
-#include <proxygen/httpserver/RequestHandler.h>
-#include <proxygen/httpserver/ResponseBuilder.h>
+#include "BaseHandler.h"
 
-#include "DataHolder.h"
-#include "Utils.h"
+namespace board {
 
-class BoardPostHandler : public proxygen::RequestHandler {
+class PostHandler : public BaseHandler {
 public:
-    utils::optional<uint64_t> extractThreadNumber(const std::string& url);
-
-    explicit BoardPostHandler(std::shared_ptr<board::DataHolder> holder, folly::fbstring prefix)
-        : prefix_(std::move(prefix)),
+    explicit PostHandler(std::shared_ptr<board::DataHolder> holder, folly::fbstring prefix)
+        : BaseHandler(prefix),
           data_holder_(holder) {
         // Nothing
     }
-
-    void onRequest(std::unique_ptr<proxygen::HTTPMessage> headers)
-        noexcept override;
-
-    void onBody(std::unique_ptr<folly::IOBuf> /*body*/) noexcept override {
-        // Nothing
-    }
-
-    void onEOM() noexcept override {
-        // Nothing
-    }
-
-    void onUpgrade(proxygen::UpgradeProtocol /*proto*/) noexcept override {
-        // Nothing
-    }
-
-    void requestComplete() noexcept override {
-        delete this;
-    }
-
-    void onError(proxygen::ProxygenError /*err*/) noexcept override {
-        delete this;
-    }
 private:
-    void handleRequest(std::unique_ptr<proxygen::HTTPMessage> headers) noexcept;
-
-    folly::fbstring prefix_;
+    void handleRequest(std::unique_ptr<proxygen::HTTPMessage> headers) noexcept override;
     std::shared_ptr<board::DataHolder> data_holder_;
 };
 
-class BoardPostHandlerFactory : public proxygen::RequestHandlerFactory {
-public:
-    BoardPostHandlerFactory(std::shared_ptr<board::DataHolder> holder, folly::fbstring prefix)
-        : prefix_(std::move(prefix)),
-          data_holder_(holder) {
-        // Nothing
-    }
-    void onServerStart(folly::EventBase* /*evb*/) noexcept override {
-        // Nothing
-    }
+using PostHandlerFactory = BaseHandlerFactory<PostHandler>;
 
-    void onServerStop() noexcept override {
-        // Nothing
-    }
-
-    proxygen::RequestHandler* onRequest(
-        proxygen::RequestHandler*,
-        proxygen::HTTPMessage*) noexcept override;
-private:
-    folly::fbstring prefix_;
-    std::shared_ptr<board::DataHolder> data_holder_;
-};
+} // namespace board
 
