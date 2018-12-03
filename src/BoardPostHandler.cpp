0cb6a1ec9b16c21040f35307f19a0683aaa20a2b
Alex <sajget@ya.ru>
2018-12-03 18:02:59 +0300

    PostHandler base implementation

--- src/BoardPostHandler.cpp
+++ src/BoardPostHandler.cpp
@@ -30,11 +30,36 @@ void PostHandler::handleRequest(
     }
     const uint64_t number = maybe_thread_number.value();
 
-    // TODO
+    auto maybe_body = getBody();
+    if (!maybe_body) {
+        // TODO: should we send anything at all?
+        ResponseBuilder(downstream_)
+            .status(500, "Internal server error")
+            .sendWithEOM();
+        return;
+    }
+
+    // TODO: extra copy here, can be removed?
+    const char* data = reinterpret_cast<const char*>(maybe_body.value()->data());
+    folly::fbstring text(data, maybe_body.value()->length());
+
+    auto result = data_holder_->addPostToThread({0, number, text, {}}).getTry();
+
+    if (result.hasException()) {
+        // TODO: It would be nice to reduce copy-paste here
+        const folly::dynamic value = folly::dynamic::object("thread", "Not Found");
+        ResponseBuilder(downstream_)
+            .status(404, "Not Found")
+            .header(HTTPHeaderCode::HTTP_HEADER_CONTENT_TYPE, "application/json")
+            .body(folly::toJson(value))
+            .sendWithEOM();
+        return;
+    }
+
     ResponseBuilder(downstream_)
         .status(200, "OK")
         .header(HTTPHeaderCode::HTTP_HEADER_CONTENT_TYPE, "application/json")
-        .body(folly::toJson(folly::dynamic::object("number", number)))
+        .body(folly::toJson(folly::dynamic::object("id", result.value())))
         .sendWithEOM();
 }
 
