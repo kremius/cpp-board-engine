c1485ba75e3a47c63e56825319d0d036967d491e
Alex <sajget@ya.ru>
2018-12-10 17:59:22 +0300

    Proper post body json parsing in PostHandler

--- src/BoardPostHandler.cpp
+++ src/BoardPostHandler.cpp
@@ -13,6 +13,18 @@
 using proxygen::ResponseBuilder;
 using proxygen::HTTPHeaderCode;
 
+namespace
+{
+
+namespace key
+{
+
+const utils::string_view TEXT("text");
+
+} // namespace key
+
+} // namespace
+
 namespace board {
 
 void PostHandler::handleRequest(
@@ -41,9 +53,18 @@ void PostHandler::handleRequest(
 
     // TODO: extra copy here, can be removed?
     const char* data = reinterpret_cast<const char*>(maybe_body.value()->data());
-    folly::fbstring text(data, maybe_body.value()->length());
+    folly::dynamic json_body = folly::parseJson(utils::string_view(data, maybe_body.value()->length()));
+    auto text_it = json_body.find(key::TEXT);
+    if (   (text_it == json_body.items().end())
+        || !text_it->second.isString()) {
+        // TODO: proper return code about bad request
+        ResponseBuilder(downstream_)
+            .status(500, "Internal server error")
+            .sendWithEOM();
+        return;
+    }
 
-    auto result = data_holder_->addPostToThread({0, number, text, {}}).getTry();
+    auto result = data_holder_->addPostToThread({0, number, text_it->second.asString(), {}}).getTry();
 
     if (result.hasException()) {
         // TODO: It would be nice to reduce copy-paste here
