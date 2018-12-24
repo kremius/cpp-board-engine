441db6b6ad7ac51035435e418d301fa530186fcb
Alex <sajget@ya.ru>
2018-12-24 14:12:33 +0300

    buildNotFoundResponse test

--- test/JsonResponseTests.cpp
+++ test/JsonResponseTests.cpp
@@ -2,14 +2,22 @@
 
 #include <proxygen/httpserver/Mocks.h>
 
-using namespace proxygen;
+using ::testing::_;
+using ::testing::Property;
+using ::testing::Invoke;
+using ::testing::Return;
+using ::testing::Sequence;
+using proxygen::MockRequestHandler;
+using proxygen::MockResponseHandler;
+using proxygen::HTTPMessage;
+using proxygen::HTTPHeaderCode;
 
 namespace {
 
 MATCHER(IsJsonContentType, "")
 {
     const auto& headers = arg.getHeaders();
-    return headers.getSingleOrEmpty(proxygen::HTTPHeaderCode::HTTP_HEADER_CONTENT_TYPE) == "application/json";
+    return headers.getSingleOrEmpty(HTTPHeaderCode::HTTP_HEADER_CONTENT_TYPE) == "application/json";
 }
 
 } // namespace
@@ -19,17 +27,7 @@ TEST(JsonResponse, BuildJsonResponse)
     MockRequestHandler request;
     MockResponseHandler response(&request);
 
-    using ::testing::Sequence;
     Sequence sequence;
-
-    proxygen::HTTPMessage message;
-
-
-    using ::testing::_;
-    using ::testing::Property;
-    using ::testing::Invoke;
-    using ::testing::Return;
-    using proxygen::HTTPMessage;
 
     const auto headers_matcher = AllOf(
         Property(&HTTPMessage::getStatusCode, 42),
@@ -39,12 +37,13 @@ TEST(JsonResponse, BuildJsonResponse)
                 sendHeaders(headers_matcher))
         .Times(1)
         .InSequence(sequence);
+
     folly::fbstring body;
-    EXPECT_CALL(response, sendBody(_)).WillOnce(
-        DoAll(
-            Invoke([&] (std::shared_ptr<folly::IOBuf> b) {
-                // TODO: use moveToFbString in other code too
-                body = b->moveToFbString();
+    EXPECT_CALL(response, sendBody(_))
+        .InSequence(sequence)
+        .WillOnce(DoAll(
+            Invoke([&] (std::shared_ptr<folly::IOBuf> buffer) {
+                body = buffer->moveToFbString();
             }),
             Return()));
 
@@ -59,3 +58,37 @@ TEST(JsonResponse, BuildJsonResponse)
     EXPECT_EQ(body, R"({"test":"json"})");
 }
 
+TEST(JsonResponse, BuildNotFoundResponse)
+{
+    MockRequestHandler request;
+    MockResponseHandler response(&request);
+
+    Sequence sequence;
+
+    const auto headers_matcher = AllOf(
+        Property(&HTTPMessage::getStatusCode, 404),
+        Property(&HTTPMessage::getStatusMessage, "Not Found"),
+        IsJsonContentType());
+    EXPECT_CALL(response,
+                sendHeaders(headers_matcher))
+        .Times(1)
+        .InSequence(sequence);
+
+    folly::fbstring body;
+    EXPECT_CALL(response, sendBody(_))
+        .InSequence(sequence)
+        .WillOnce(DoAll(
+            Invoke([&] (std::shared_ptr<folly::IOBuf> buffer) {
+                body = buffer->moveToFbString();
+            }),
+            Return()));
+
+    EXPECT_CALL(response, sendEOM())
+        .Times(1)
+        .InSequence(sequence);
+
+    request.setResponseHandler(&response);
+
+    board::buildNotFoundResponse(&response);
+    EXPECT_EQ(body, "{}");
+}
