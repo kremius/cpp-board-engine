9c1cfedb6ec6547dff1fff823a3ac69d3da20f76
Alex <sajget@ya.ru>
2019-01-11 15:11:10 +0300

    'Not Found' branches have been covered by tests for GetThreadHandler

--- test/GetThreadHandlerTests.cpp
+++ test/GetThreadHandlerTests.cpp
@@ -22,42 +22,101 @@ MATCHER(IsJsonContentType, "") {
 
 class GetThreadHandlerTest : public ::testing::Test {
 protected:
-    GetThreadHandlerTest() {
-        holder_ = std::make_shared<board::DataHolder>();
+    GetThreadHandlerTest()
+        : holder_(std::make_shared<board::DataHolder>()),
+          handler_(holder_, "/test/"),
+          response_mock_(&handler_) {
+        handler_.setResponseHandler(&response_mock_);
         holder_->createThread({0, 0, "text1", "image1"}).getTry();
     };
     std::shared_ptr<board::DataHolder> holder_;
+    board::GetThreadHandler handler_;
+    proxygen::MockResponseHandler response_mock_;
 };
 
 TEST_F(GetThreadHandlerTest, Basics) {
-    board::GetThreadHandler handler(holder_, "/test/");
-    proxygen::MockResponseHandler response_mock(&handler);
-    handler.setResponseHandler(&response_mock);
-
     Sequence sequence;
 
     const auto headers_matcher = AllOf(
         Property(&HTTPMessage::getStatusCode, 200),
         IsJsonContentType());
-    EXPECT_CALL(response_mock,
+    EXPECT_CALL(response_mock_,
                 sendHeaders(headers_matcher))
         .Times(1)
         .InSequence(sequence);
     folly::fbstring body;
-    EXPECT_CALL(response_mock, sendBody(_))
+    EXPECT_CALL(response_mock_, sendBody(_))
         .InSequence(sequence)
         .WillOnce(DoAll(
             Invoke([&] (std::shared_ptr<folly::IOBuf> buffer) {
                 body = buffer->moveToFbString();
             }),
             Return()));
-    EXPECT_CALL(response_mock, sendEOM())
+    EXPECT_CALL(response_mock_, sendEOM())
         .Times(1)
         .InSequence(sequence);
 
     auto headers = std::make_unique<proxygen::HTTPMessage>();
     headers->setURL("/test/1");
-    handler.onRequest(std::move(headers));
+    handler_.onRequest(std::move(headers));
     folly::EventBaseManager::get()->getEventBase()->loop();
     EXPECT_EQ(body, R"({"posts":[{"text":"text1","id":1}]})");
+}
+
+TEST_F(GetThreadHandlerTest, InvalidThreadId) {
+    Sequence sequence;
+
+    const auto headers_matcher = AllOf(
+        Property(&HTTPMessage::getStatusCode, 404),
+        IsJsonContentType());
+    EXPECT_CALL(response_mock_,
+                sendHeaders(headers_matcher))
+        .Times(1)
+        .InSequence(sequence);
+    folly::fbstring body;
+    EXPECT_CALL(response_mock_, sendBody(_))
+        .InSequence(sequence)
+        .WillOnce(DoAll(
+            Invoke([&] (std::shared_ptr<folly::IOBuf> buffer) {
+                body = buffer->moveToFbString();
+            }),
+            Return()));
+    EXPECT_CALL(response_mock_, sendEOM())
+        .Times(1)
+        .InSequence(sequence);
+
+    auto headers = std::make_unique<proxygen::HTTPMessage>();
+    headers->setURL("/test/notanumber");
+    handler_.onRequest(std::move(headers));
+    folly::EventBaseManager::get()->getEventBase()->loop();
+    EXPECT_EQ(body, "{}");
+}
+
+TEST_F(GetThreadHandlerTest, ThreadDoesNotExist) {
+    Sequence sequence;
+
+    const auto headers_matcher = AllOf(
+        Property(&HTTPMessage::getStatusCode, 404),
+        IsJsonContentType());
+    EXPECT_CALL(response_mock_,
+                sendHeaders(headers_matcher))
+        .Times(1)
+        .InSequence(sequence);
+    folly::fbstring body;
+    EXPECT_CALL(response_mock_, sendBody(_))
+        .InSequence(sequence)
+        .WillOnce(DoAll(
+            Invoke([&] (std::shared_ptr<folly::IOBuf> buffer) {
+                body = buffer->moveToFbString();
+            }),
+            Return()));
+    EXPECT_CALL(response_mock_, sendEOM())
+        .Times(1)
+        .InSequence(sequence);
+
+    auto headers = std::make_unique<proxygen::HTTPMessage>();
+    headers->setURL("/test/2");
+    handler_.onRequest(std::move(headers));
+    folly::EventBaseManager::get()->getEventBase()->loop();
+    EXPECT_EQ(body, "{}");
 }
